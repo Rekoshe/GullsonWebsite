24c043f35c16b1b64859a6dc6d73287276c60642
Reko <42325581+Rekoshe@users.noreply.github.com>
2023-08-10 15:45:41 +0300

    fixed navbar on mobile

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,18 +4,19 @@ import Styles from './navbar.module.scss';
 import Image from 'next/image';
 import NavbarText from './navbarText';
 import Link from "next/link";
-import {basePath} from '../../../next.config';
+import { basePath } from '../../../next.config';
 
 
 export default function Navbar() {
 
-    
+
 
     return (
-        <nav className={Styles.container}>
-            <div className={Styles.blackSection}>
-                <div className={Styles.blackSectionContent}>
-                    <div className={Styles.text}>
+        <nav className={Styles.section}>
+            <div className={Styles.container}>
+                <div className={Styles.blackSection}>
+                    <div className={Styles.blackSectionContent}>
+
                         <NavbarText icon='location_on' text='7th Circle ,RJ Passengers Complex'></NavbarText>
                         <NavbarText icon='call' text='Tel: +962 6 5821019'></NavbarText>
                         <NavbarText icon='email' text='Email: info@gullsun.com'></NavbarText>
@@ -27,31 +28,33 @@ export default function Navbar() {
                         <a href="#" className={["fa", "fa-envelope", Styles.socialIcon].join(" ")}></a>
 
                         <button className={Styles.button}>get a qoute</button>
+
+
+
                     </div>
-
-
                 </div>
-            </div>
-            <div className={Styles.whiteSection}>
-                <div className={Styles.whiteSectionContent}>
-                    <div className={Styles.logo}>
-                        <a href="#">
-                            <Image src={`${basePath}/images/logoProper.jpg`} alt="gullson logo" height='75' width='75'></Image>
-                        </a>
-                    </div>
-                    <div className={Styles.navItem}>
-                        <NavbarItem href='/' name={'home'}></NavbarItem>
-                        <NavbarItem href='#' name={'services'}>
-                            
-                        </NavbarItem>
-                        <NavbarItem href='#' name={'projects'}></NavbarItem>
-                        <NavbarItem href='#' name={'contact us'}></NavbarItem>
-                        <NavbarItem href='#' name={'about us'}></NavbarItem>
-                        <button className={[Styles.navButton, 'material-icons'].join(" ")}>search</button>
-                        <button className={[Styles.navButton, 'material-icons'].join(" ")}>menu</button>
-                        <div className={Styles.dropdownContent}>
-                            <Link href='/ocean-freight'className={Styles.dropdownItem}>ocean freight</Link>
-                            <Link href='#'className={Styles.dropdownItem}>air freight</Link>
+                <div className={Styles.whiteSection}>
+                    <div className={Styles.whiteSectionContent}>
+                        <div className={Styles.logo}>
+                            <a href="#">
+                                <Image src={`${basePath}/images/logoProper.jpg`} alt="gullson logo" height='75' width='75'></Image>
+                            </a>
+                        </div>
+                        <div className={Styles.navItem}>
+                            <NavbarItem href='/' name={'home'}></NavbarItem>
+                            <NavbarItem href='#' name={'services'}>
+
+                            </NavbarItem>
+                            <NavbarItem href='#' name={'projects'}></NavbarItem>
+                            <NavbarItem href='#' name={'contact us'}></NavbarItem>
+                            <NavbarItem href='#' name={'about us'}></NavbarItem>
+                            <button className={[Styles.navButton, 'material-icons'].join(" ")}>search</button>
+                            <button className={[Styles.navButton, 'material-icons'].join(" ")}>menu</button>
+                            <button className={['material-icons', Styles.mobileNavButton].join(" ")}>menu</button>
+                            <div className={Styles.dropdownContent}>
+                                <Link href='/ocean-freight' className={Styles.dropdownItem}>ocean freight</Link>
+                                <Link href='#' className={Styles.dropdownItem}>air freight</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
