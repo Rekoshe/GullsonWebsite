8526721a4984e1e42219536fbd4dd0f07495245c
Reko <42325581+Rekoshe@users.noreply.github.com>
2023-08-13 12:54:11 +0300

    added animations for inquiry section

--- src/components/Inquiry/inquiry.jsx
+++ src/components/Inquiry/inquiry.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Image from "next/image";
 import Styles from './inquiry.module.scss'
-import { styled } from "styled-components";
+import { motion } from 'framer-motion'
 import { basePath } from '../../../next.config';
 
 export default function Inquiry() {
+
+
     return (
         <div className={Styles.section}>
             <div className={Styles.content}>
-                <div>
-                    <p className={Styles.smalltext}>Fast & reliable</p>
-                    <h1 className={Styles.bigText}>Transport inquiry</h1>
-                    <p className={Styles.paragraph}>Send us your transport inquiry – we are pleased to send you an offer without obligation. Our solutions are tailored on your individual requirements.</p>
-                    <button className={Styles.requestButton}>send request</button>
+                <div className={Styles.contentGrid}>
+                    <motion.div whileInView={{clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)'}}
+                    transition={{duration: 1,}}
+                    viewport={{once: true}}
+                     className={Styles.bracket}>{'['}</motion.div>
+                    <div className={Styles.text}>
+                        <p className={Styles.smalltext}>Fast & reliable</p>
+                        <motion.h1 whileInView={{ transform: 'translateX(0)', opacity: 1 }}
+                            transition={{delay: 1, duration: 1}}
+                            viewport={{ once: true }}
+                         className={Styles.bigText}>Transport inquiry</motion.h1>
+                        <p className={Styles.paragraph}>Send us your transport inquiry – we are pleased to send you an offer without obligation. Our solutions are tailored on your individual requirements.</p>
+                        <button className={Styles.requestButton}>send request</button>
+                    </div>
                 </div>
             </div>
             <Image className={Styles.background} src={`${basePath}/images/section_back_1.jpg`} alt="image" fill></Image>
--- src/components/Services/serviceCard.jsx
+++ src/components/Services/serviceCard.jsx
@@ -10,7 +10,8 @@ export default function serviceCard(props) {
     return (
         <motion.div whileInView={{transform: 'translateY(0)',opacity: 1}}
          className={[Styles.cardItem, 'col-sm-4'].join(" ")}
-         transition={{delay: 0.3, duration: 0.5}}>
+         transition={{delay: 0.3, duration: 0.5}}
+         viewport={{ once: true}}>
             <span className={['material-icons', Styles.cardIcon].join(" ")}>{props.icon}</span>
             <h1 className={Styles.cardTitle}>{props.title}</h1>
             <p className={Styles.cardParagraph}>{props.paragraph}</p>
